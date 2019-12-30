8bf360f85914f89d6ab7bda97e93794bd4bca2dd
huang7000 <1621393570@qq.com>
2019-12-31 01:20:24 +0800

    20191230提交动态 组件

---
+++ src/demos/vuedoc/MyComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hv-snippet">
+    <h2 class="hv-snippet--title">我是MyComponent组件</h2>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { title: "我是MyComponent组件" };
+  },
+   template: "<h3>我是MyComponent组件</h3>"
+};
+</script>
--- src/demos/vuedoc/my-async-component.js
+++ src/demos/vuedoc/my-async-component.js
@@ -1,1 +1,4 @@
- var template = "wo wd ";
+var myComponent = {
+    template: "<h3>my-async-component</h3>"
+  };
+export default myComponent
